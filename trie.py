e2d01baf09fa2a619a789c21c65090a35a9c091a
Carl Handlin <carl.wallace@konfio.mx>
2019-01-12 18:03:17 -0600

    Added trie structure

---
+++ trie.py
@@ -0,0 +1,48 @@
+# Implement a Trie using a dictionary
+
+#      t
+#    /   \
+#   h     e
+#    \   / \
+#     e a   n
+
+
+class Trie():
+
+    def __init__(self):
+        self.trie = dict()
+
+    def __repr__(self):
+        return "Trie(%s)" % self.trie
+
+    def insert(self, word):
+        if self.find(word):
+            pass
+        else:
+            trie = self.trie
+
+            temp_dict = trie
+            for letter in word:
+                temp_dict = temp_dict.setdefault(letter, {})
+
+            self.trie = trie
+
+    def find(self, word):
+        temp_dict = self.trie
+
+        for letter in word:
+            if letter in temp_dict.keys():
+                temp_dict = temp_dict[letter]
+            else:
+                return False
+
+        return True
+
+
+trie = Trie()
+print(trie)
+trie.insert('tea')
+trie.insert('the')
+trie.insert('ten')
+print(trie)
+print(trie.find('ten'))
